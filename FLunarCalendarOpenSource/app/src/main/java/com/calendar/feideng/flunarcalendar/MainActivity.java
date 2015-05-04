046f3b10b45a48200e1c06a38e3ca72f56a55eb3
FeiDeng <fdeng2014@gmail.com>
2015-05-04 00:01:41 -0700

    add MonthFragment

---
+++ FLunarCalendarOpenSource/app/src/main/java/com/calendar/feideng/flunarcalendar/MainActivity.java
@@ -0,0 +1,98 @@
+package com.calendar.feideng.flunarcalendar;
+
+
+import android.app.ActionBar;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends FragmentActivity {
+
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Configure action bar
+        ActionBar actionBar = getActionBar();
+        // Enable navigating up with app icon
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        // Disable app icon
+        actionBar.setDisplayShowHomeEnabled(false);
+        actionBar.setDisplayShowTitleEnabled(true);
+        actionBar.setTitle("My Profile");
+        actionBar.setDisplayUseLogoEnabled(false);
+
+
+        // Init ViewPager
+        viewPager = (ViewPager)findViewById(R.id.id_viewpager);
+        // Declare an Adapter for ViewPager
+        CalendarViewPagerAdapter viewPagerAdapter = new CalendarViewPagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(viewPagerAdapter);
+        viewPager.setCurrentItem(500);
+
+    }
+
+    // ViewPager adapter used to talk with Fragments
+    public class CalendarViewPagerAdapter extends FragmentPagerAdapter {
+
+        public CalendarViewPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+           return MonthFragment.create(position);
+        }
+
+        @Override
+        public int getCount() {
+            return 1000;
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.action_today:
+                return true;
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /* Click actions for bottom textView */
+    /*
+    public void onTodayClick(View view) {
+        Toast.makeText(MainActivity.this, "Today click", Toast.LENGTH_LONG).show();
+    }
+
+    public void onCalendarsClick(View view) {
+        Toast.makeText(MainActivity.this, "Calendars click", Toast.LENGTH_LONG).show();
+    }
+
+    public void onInboxClick(View view) {
+        Toast.makeText(MainActivity.this, "Inbox click", Toast.LENGTH_LONG).show();
+    }
+    */
+}
---
+++ FLunarCalendarOpenSource/app/src/main/java/com/calendar/feideng/flunarcalendar/MonthFragment.java
@@ -0,0 +1,63 @@
+package com.calendar.feideng.flunarcalendar;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MonthFragment extends Fragment {
+
+    /**
+     * The argument key for the page number this fragment represents.
+     */
+    public static final String ARG_PAGE = "page";
+
+    /**
+     * The fragment's page number, which is set to the argument value for {@link #ARG_PAGE}.
+     */
+    private int mPageNumber;
+
+    /**
+     * Factory method for this fragment class. Constructs a new fragment for the given page number.
+     */
+    public static MonthFragment create(int pageNumber) {
+        MonthFragment monthFragment = new MonthFragment();
+        Bundle bundle = new Bundle();
+        bundle.putInt(ARG_PAGE, pageNumber);
+        monthFragment.setArguments(bundle);
+        return monthFragment;
+    }
+
+
+    public MonthFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mPageNumber = getArguments().getInt(ARG_PAGE);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        TextView textView = new TextView(getActivity());
+        textView.setText(R.string.hello_blank_fragment);
+        return textView;
+    }
+
+    /**
+     * Returns the page number represented by this fragment object.
+     */
+    public int getPageNumber() {
+        return mPageNumber;
+    }
+}
