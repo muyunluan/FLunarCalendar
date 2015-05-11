94699afacb707dcec47fb1beb4e2e9c47a6a3e3a
FeiDeng <fdeng2014@gmail.com>
2015-05-10 21:41:17 -0700

    update

--- FLunarCalendarOpenSource/app/src/main/java/com/calendar/feideng/flunarcalendar/MonthFragment.java
+++ FLunarCalendarOpenSource/app/src/main/java/com/calendar/feideng/flunarcalendar/MonthFragment.java
@@ -1,14 +1,12 @@
 package com.calendar.feideng.flunarcalendar;
 
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
-import android.widget.TextView;
 
 import java.util.Calendar;
 
